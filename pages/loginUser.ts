82ea1be8b7a8e89df3a37a8318ae5a15d326ba3a
marjgermano <marjo.germano@gmail.com>
2026-04-23 13:43:31 +1200

    Refactor TC1-4

--- pages/loginUser.ts
+++ pages/loginUser.ts
@@ -1,25 +1,3 @@
-// import { Page, Locator } from '@playwright/test';
-
-// export class LoginUser {
-//     readonly page: Page;
-//     readonly loginEmail: Locator;
-//     readonly loginPassword: Locator;
-//     readonly loginBtn: Locator;
-
-//     constructor(page: Page) {
-//         this.page = page;
-//         this.loginEmail = page.getByTestId('login-email');
-//         this.loginPassword = page.getByTestId('login-password');
-//         this.loginBtn = page.getByRole('button', { name: 'Login' });
-//     }
-
-//     async login(email: string, pass: string) {
-//         await this.loginEmail.fill(email);
-//         await this.loginPassword.fill(pass);
-//         await this.loginBtn.click();
-//     }
-// }
-
 import { Page } from "@playwright/test";
 
 export class LoginUser {
