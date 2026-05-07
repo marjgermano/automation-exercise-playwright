e89d29dcb8a397a6e0a0d8e1243fda6ffddaf24a
Marj Germano <marjo.germano@gmail.com>
2026-05-07 16:05:08 +1200

    fix: Use baseTest

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "./baseTest";
 
 test("TC7: Verify Test Cases page", async ({ page }) => {
   await page.goto("/", { waitUntil: "load" });
