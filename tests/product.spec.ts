a3508dde75a96f459898e49abeea3a7f65d64a35
Marj Germano <marjo.germano@gmail.com>
2026-05-16 22:18:16 +1200

    add comment and update RTM

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -20,34 +20,22 @@ test.describe("Product Catalog (TC 8-9)", () => {
     await productDetails.verifyProductDetailsVisible();
   });
 
-  //   test("TC9: Search product", async ({ page }) => {
-  //     const productsPage = new AllProducts(page);
-
-  //     await productsPage.navigateTo();
-  //     await expect(productsPage.allProductsHeader).toBeVisible();
-  //     await productsPage.searchForProduct('Blue Top')
-  //     await expect(productsPage.searchedProductsHeader).toBeVisible()
-  //   });
-
   test("TC9: Search product", async ({ page }) => {
     const productsPage = new AllProducts(page);
     const searchKeyword = "Blue Top";
 
-    // 1-5. Navigate and Verify "All Products"
     await productsPage.navigateTo();
     await expect(productsPage.allProductsHeader).toBeVisible();
 
-    // 6. Enter product name and click search
     await productsPage.searchForProduct(searchKeyword);
 
-    // 7. Verify 'SEARCHED PRODUCTS' is visible
     await expect(productsPage.searchedProductsHeader).toBeVisible();
 
-    // 8. Verify all products related to search are visible
-    // We get the collection of all product names in the search results
+    // Verify all products related to search are visible
+    // Get the collection of all product names in the search results
     const products = productsPage.searchResultNames;
 
-    // Safety check: Ensure at least one product was foundfcd[=-c vv]
+    // Ensure at least one product was found
     await expect(products).not.toHaveCount(0);
 
     // Loop through every product found and verify the text
