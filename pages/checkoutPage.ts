c16f42766ca9be4e2d3255d5028dacd976bfe4bb
marjgermano <marjo.germano@gmail.com>
2026-06-05 23:31:33 -0700

    fix

--- pages/checkoutPage.ts
+++ pages/checkoutPage.ts
@@ -23,7 +23,7 @@ export class CheckoutPage {
   }
 
   get downloadInvoiceBtn() {
-    return this.page.getByRole('link', { name: 'Download Invoice' });
+    return this.page.getByRole("link", { name: "Download Invoice" });
   }
 
   get continueBtn() {
@@ -55,13 +55,15 @@ export class CheckoutPage {
 
   async downloadInvoice(firstName: string) {
     // Set up the download event listener promise thread before clicking
-    const downloadPromise = this.page.waitForEvent('download');
+    const downloadPromise = this.page.waitForEvent("download", {
+      timeout: 60000,
+    });
     await this.downloadInvoiceBtn.click();
     const download = await downloadPromise;
-    
+
     // Assert that the file downloaded successfully with expected extension type
-    expect(download.suggestedFilename()).toContain('.txt');
-    
+    expect(download.suggestedFilename()).toContain(".txt");
+
     // Save the downloaded file into a dedicated execution workspace folder
     await download.saveAs(`./test-results/invoice_${firstName}.txt`);
   }
